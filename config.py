f341198420ed594b15528007313d7c5e8481d871
zhangyi-zjuer <zhangyi.zjuer@gmail.com>
2014-03-18 18:04:27 +0800

    remove unused code

--- config.py
+++ config.py
@@ -1,9 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
 
-NGINX_CONFIG_DIR = '/etc/nginx/sites-enabled/'
-NGINX_RELOAD_CMD = '/usr/sbin/nginx -s reload'
-
 BOOTSTRAP_SERVE_LOCAL = True
 SECRET_KEY = 'Bon-Jovi-Have-a-Nice-Day'
 
